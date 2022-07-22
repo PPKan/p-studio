1789589ce9620457c80ef6e2c29695b528b0a32c
Peter <petervsjim@gmail.com>
2022-07-23 00:21:58 +0800

    add some backgroudns

--- src/pages/Front/Front.tsx
+++ src/pages/Front/Front.tsx
@@ -3,26 +3,32 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import "../../css/Front/Front.scss";
 import FrontServices from "./FrontServices";
-import FrontShow from './FrontShow';
-import FrontWorks from './FrontWorks';
-import FrontInformation from './FrontInformation';
+import FrontShow from "./FrontShow";
+import FrontWorks from "./FrontWorks";
+import FrontInformation from "./FrontInformation";
+import bgicon from "../../images/bg-icon.svg";
 
 type Props = {};
 
 export default function Front({}: Props) {
   return (
     <>
-    <Navbar/>
-    <div className="spacer--16h"></div>
-    <FrontShow />
-    <div className="spacer--16h"></div>
-    <FrontServices />
-    <div className="spacer--16h"></div>
-    <FrontWorks />
-    <div className="spacer--16h"></div>
-    <FrontInformation />
-    <div className="spacer--16h"></div>
-    <Footer />
+      <Navbar />
+      <div className="spacer--16h"></div>
+      <FrontShow />
+      <div className="spacer--16h">
+      <img className="spacer__background--left" src={bgicon} alt=""/>
+      </div>
+      <FrontServices />
+      <div className="spacer--16h">
+      <img className="spacer__background--right" src={bgicon} alt=""/>
+      </div>
+      
+      <FrontWorks />
+      <div className="spacer--16h"></div>
+      <FrontInformation />
+      <div className="spacer--16h"></div>
+      <Footer />
     </>
   );
 }

f327db06702d39e85b20756d8dbbec0aa2088779
Peter Work Laptop <petervsjim@gmail.com>
2022-07-29 17:22:23 +0800

    add link to front contact

--- src/pages/Front/FrontContact.tsx
+++ src/pages/Front/FrontContact.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { Link, Navigate } from "react-router-dom";
 import "../../css/Front/FrontContact.scss";
 
 type Props = {};
 
 export default function FrontContact({}: Props) {
+
   return (
     <section className="front__contact">
-      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+      <Link to="./contact">
         <div className="front__contact__container">
           <img className="front__contact__key" src="src/images/key-270.png" />
           <img className="front__contact__lock" src="src/images/lock.png" />
         </div>
-      </a>
+      </Link>
     </section>
   );
 }

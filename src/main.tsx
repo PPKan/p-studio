54429c224e9646a429c69ef488813d6f13ae2762
Peter Work Laptop <petervsjim@gmail.com>
2022-08-11 17:08:13 +0800

    change back to browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import About from "./pages/About/About";
 import Works from "./pages/Works/Works";

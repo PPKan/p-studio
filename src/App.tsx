3c2a4104e026d595f2ddea1fe825df82af88037e
Peter Work Laptop <petervsjim@gmail.com>
2022-08-17 16:29:09 +0800

    change routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import "./css/reset.css";
 import "./css/App.scss";
+import About from "./pages/About/About";
+import Works from "./pages/Works/Works";
+import Services from "./pages/Services/Services";
+import Info from "./pages/Info/Info";
+import Contact from "./pages/Contact/Contact";
+import ScrollToTop from "./components/ScrollToTop";
 import Front from "./pages/Front/Front";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Front />
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Front />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/works" element={<Works />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/info" element={<Info />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import About from "./pages/About/About";
-import Works from "./pages/Works/Works";
-import Services from "./pages/Services/Services";
-import Info from "./pages/Info/Info";
-import Contact from "./pages/Contact/Contact";
-import ScrollToTop from "./components/ScrollToTop";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <ScrollToTop />
-    <Routes>
-      <Route path="/" element={<App />}></Route>
-      <Route path="/about" element={<About />}></Route>
-      <Route path="/works" element={<Works />}></Route>
-      <Route path="/services" element={<Services />}></Route>
-      <Route path="/info" element={<Info />}></Route>
-      <Route path="/contact" element={<Contact />}></Route>
-    </Routes>
-  </BrowserRouter>
+  <>
+    <App />
+  </>
 );
